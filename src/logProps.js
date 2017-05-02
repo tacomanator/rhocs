a14efefd671a5e2511d2a1db38b9d824e1213bbd
Drew Goodwin <drew@drewgoodwin.com>
2017-05-02 16:56:02 -0700

    logProps: logs only selected props

--- src/logProps.js
+++ src/logProps.js
@@ -1,21 +1,20 @@
 import React from "react";
 import R from "ramda";
 
-export default selector =>
-  WrappedComponent => {
-    let finalSelector;
-    if (R.isNil(selector)) {
-      finalSelector = R.identity;
-    } else if (selector instanceof Array) {
-      finalSelector = R.props(propsSelector);
-    } else if (typeof selector === "string") {
-      finalSelector = R.prop(propsSelector);
-    } else if (typeof selector === "function") {
-      finalSelector = propsSelector;
-    }
+export default selector => WrappedComponent => {
+  let finalSelector;
+  if (R.isNil(selector)) {
+    finalSelector = R.identity;
+  } else if (selector instanceof Array) {
+    finalSelector = R.props(propsSelector);
+  } else if (typeof selector === "string") {
+    finalSelector = R.prop(propsSelector);
+  } else if (typeof selector === "function") {
+    finalSelector = propsSelector;
+  }
 
-    return props => {
-      console.log(WrappedComponent.name, props, finalSelector(props));
-      return <WrappedComponent {...props} />;
-    };
+  return props => {
+    console.log(WrappedComponent.name, finalSelector(props));
+    return <WrappedComponent {...props} />;
   };
+};
